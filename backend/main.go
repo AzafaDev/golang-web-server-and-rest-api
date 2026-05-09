2b934799383b0fd1350495737efd6bded5877374
Akmal Dzakwan Faiz <akmal.dz.f@gmail.com>
2026-05-09 10:22:17 +0700

    feat: initialize frontend with Next.js, TypeScript, and Tailwind CSS
    - Added package.json for frontend dependencies and scripts.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added SVG assets: file.svg, globe.svg, next.svg, vercel.svg, window.svg.
    - Configured TypeScript with tsconfig.json for strict type checking and Next.js support.

---
+++ backend/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	connStr := os.Getenv("DATABASE_URL")
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(context.Background())
+}
